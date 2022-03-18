5e0463c6221ad226cb312385f04f0c3dc28f3dfe
Devamchaudhari <devamchaudhari40@gmail.com>
2022-03-18 09:28:06 +0530

    intial commit

---
+++ assets/script/raptor.js
@@ -0,0 +1,23 @@
+var MenuItems = document.getElementById("nav-list");
+
+function myBurger() {
+    console.log('test-----------', MenuItems.classList);
+
+    if (MenuItems.classList.length === 1) {
+        MenuItems.classList.add("nav-list--xs");
+
+    } else {
+        MenuItems.classList.remove("nav-list--xs");
+        MenuItems.style.display = "none";
+    }
+}
+
+window.addEventListener('scroll', function () {
+    let nav = document.getElementById("nav")
+    let windowscroll = window.scrollY > 420;
+    if (window.scrollY > 420) {
+        nav.classList.toggle('navbar-scroll', windowscroll);
+    } else {
+        nav.classList.remove("navbar-scroll");
+    }
+})
